52990bd9c179c1c656e13281176d9a4184cd0d4d
aevdokimov <aevdokimov@itqua.ru>
2019-07-01 21:05:44 +1000

    3

--- index.php
+++ index.php
@@ -10,14 +10,10 @@ $me = $MadelineProto->get_self();
 \danog\MadelineProto\Logger::log($me);
 
 if (!$me['bot']) {
-    $MadelineProto->messages->sendMessage(['peer' => '@danogentili', 'message' => "Hi!\nThanks for creating MadelineProto! <3"]);
-    $MadelineProto->channels->joinChannel(['channel' => '@MadelineProto']);
+    $MadelineProto->messages->sendMessage([
+        'peer' => 79502901932,
+        'message' => "test"
+    ]);
 
-    try {
-        $MadelineProto->messages->importChatInvite(['hash' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg']);
-    } catch (\danog\MadelineProto\RPCErrorException $e) {
-    }
-
-    $MadelineProto->messages->sendMessage(['peer' => 'https://t.me/joinchat/Bgrajz6K-aJKu0IpGsLpBg', 'message' => 'Testing MadelineProto!']);
 }
 echo 'OK, done!'.PHP_EOL;
