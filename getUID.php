7c507e3d97c579b0875bca4155112e9d8da1eb7a
aevdokimov <aevdokimov@itqua.ru>
2019-07-04 18:05:18 +1000

    userid

---
+++ getUID.php
@@ -0,0 +1,20 @@
+<?php
+
+$data_string = "tt=54";
+$token = "614983231:AAFr-IYreAkrKyFczPK00XAa4mzJGNoTZPI";
+$url = "https://api.telegram.org/";
+$chat_id = 727792637;
+$phone_number = '+79502901932';
+$first_name = 't';
+
+$tUrl = $url.$token.'/sendContact?';
+$tUrl .= 'chat_id='.$chat_id;
+$tUrl .= '&phone_number='.$phone_number;
+$tUrl .= '&first_name='.$first_name;
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $tUrl);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$answer = curl_exec($curl);
+
+echo $answer;
