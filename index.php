462311a554af0e4b848c3a56bc62802e1f901844
aevdokimov <aevdokimov@itqua.ru>
2019-07-05 10:41:00 +1000

    1

--- index.php
+++ index.php
@@ -10,16 +10,22 @@ $me = $MadelineProto->start();
 $me = $MadelineProto->get_self();
 
 \danog\MadelineProto\Logger::log($me);
+$phone_number = $_GET("PN");
 
-if (!$me['bot']) {
-
+if isset($phone_number){
     $uid = getUID('+79502901932');
-
+    if ($uid == null){
+        echo "Неправильный номер";
+        exit;
+    }
     $MadelineProto->messages->sendMessage([
         'peer' => $uid,
         'message' => "Отправка по номеру телефонв"
     ]);
+    echo 'OK, done!';
+} else {
+    echo "Введите номер телефона!"
 }
 
 
-echo 'OK, done!'.PHP_EOL;
+
