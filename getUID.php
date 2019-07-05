431e5572e9313539d7b8e1727835b90918b676ed
aevdokimov <aevdokimov@itqua.ru>
2019-07-05 11:11:00 +1000

    1

--- getUID.php
+++ getUID.php
@@ -4,6 +4,7 @@ function getUID($phone_number){
     $token = "614983231:AAFr-IYreAkrKyFczPK00XAa4mzJGNoTZPI";
     $url = "https://api.telegram.org/bot";
     $chat_id = 727792637;
+    $res = null;
 //    $phone_number = '+79502901932';
     $first_name = 't';
 
@@ -20,14 +21,15 @@ function getUID($phone_number){
     $answer = curl_exec($curl);
 
 //    $answer = '{"ok":true,"result":{"message_id":99,"from":{"id":614983231,"is_bot":true,"first_name":"ainarry_bot","username":"ainarry_bot"},"chat":{"id":727792637,"first_name":"\u0410\u043b\u0435\u043a\u0441\u0430\u043d\u0434\u0440","username":"Fogmart","type":"private"},"date":1562233806,"contact":{"phone_number":"79502901932","first_name":"t","user_id":727792637}}}';
-
+//    $answer = '{"ok":true,"result":{"message_id":106,"from":{"id":614983231,"is_bot":true,"first_name":"ainarry_bot","username":"ainarry_bot"},"chat":{"id":727792637,"first_name":"\u0410\u043b\u0435\u043a\u0441\u0430\u043d\u0434\u0440","username":"Fogmart","type":"private"},"date":1562288748,"contact":{"phone_number":"95029019","first_name":"t"}}}';
     $answer = json_decode($answer);
 
     if ($answer->ok) {
-        return $answer->result->contact->user_id;
-    } else{
-        return null;
+        if (isset($answer->result->contact->user_id)) {
+            $res = $answer->result->contact->user_id;
+        }
     }
+    return $res;
 
 }
 
